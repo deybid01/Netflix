7b37721ce653eb57e4cfb474ee4f8a5908b283b7
David Christian Sopot <79466327+deybid01@users.noreply.github.com>
2022-05-06 10:18:19 +0800

    testing blank href

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -9,58 +9,58 @@ const Footer = () => {
             <div className='footer-cols'>
                 <ul>
                     <li>
-                        <a href='#'>FAQ</a>
+                        <a>FAQ</a>
                     </li>
                     <li>
-                        <a href='#'>Investor Relations</a>
+                        <a>Investor Relations</a>
                     </li>
                     <li>
-                        <a href='#'>Corporate Information</a>
+                        <a>Corporate Information</a>
                     </li>
                     <li>
-                        <a href='#'>Netflix Originals</a>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <a href='#'>Help Center</a>
-                    </li>
-                    <li>
-                        <a href='#'>Jobs</a>
-                    </li>
-                    <li>
-                        <a href='#'>Term of Use</a>
-                    </li>
-                    <li>
-                        <a href='#'>Contact Us</a>
+                        <a>Netflix Originals</a>
                     </li>
                 </ul>
                 <ul>
                     <li>
-                        <a href='#'>Account</a>
+                        <a>Help Center</a>
                     </li>
                     <li>
-                        <a href='#'>Redeem Gift Cards</a>
+                        <a>Jobs</a>
                     </li>
                     <li>
-                        <a href='#'>Privacy</a>
+                        <a>Term of Use</a>
                     </li>
                     <li>
-                        <a href='#'>Speed Test</a>
+                        <a>Contact Us</a>
                     </li>
                 </ul>
                 <ul>
                     <li>
-                        <a href='#'>Media Center</a>
+                        <a>Account</a>
                     </li>
                     <li>
-                        <a href='#'>Buy Gift Cards</a>
+                        <a>Redeem Gift Cards</a>
                     </li>
                     <li>
-                        <a href='#'>Cookie Preferences</a>
+                        <a>Privacy</a>
                     </li>
                     <li>
-                        <a href='#'>Legal Notices</a>
+                        <a>Speed Test</a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a>Media Center</a>
+                    </li>
+                    <li>
+                        <a>Buy Gift Cards</a>
+                    </li>
+                    <li>
+                        <a>Cookie Preferences</a>
+                    </li>
+                    <li>
+                        <a>Legal Notices</a>
                     </li>
                 </ul>
             </div>
